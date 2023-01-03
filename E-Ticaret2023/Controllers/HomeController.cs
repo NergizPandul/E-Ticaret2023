a16fd6ce10b617db302869f02b213fd3b2100b85
NergizPandul <nergiz.pandul@gmail.com>
2023-01-03 14:05:26 +0300

    sepet

--- E-Ticaret2023/Controllers/HomeController.cs
+++ E-Ticaret2023/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_Ticaret2023.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +8,26 @@ using System.Web.Mvc;
 namespace E_Ticaret2023.Controllers
 {
     public class HomeController : Controller
-    {
+
+    {   Entities db = new Entities();
         public ActionResult Index()
         {
+
+            ViewBag.Kategoriler=db.Kategoriler.ToList();
+            ViewBag.Urunler=db.Urunler.ToList();
             return View();
+        }
+        public ActionResult Kategori(int id)
+        {
+            ViewBag.Kategoriler = db.Kategoriler.ToList();
+            ViewBag.Kategori = db.Kategoriler.Find(id);
+            return View(db.Urunler.Where(x=>x.KategoriID==id).ToList());
+        }
+
+        public ActionResult Urun(int id)
+        {
+            ViewBag.Kategoriler = db.Kategoriler.ToList();
+            return View(db.Urunler.Find(id));
         }
 
         public ActionResult About()
---
+++ E-Ticaret2023/Controllers/SepetController.cs
@@ -0,0 +1,47 @@
+﻿using E_Ticaret2023.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Ticaret2023.Controllers
+{
+    [Authorize]
+    public class SepetController : Controller
+    {
+       Entities db=new Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SepeteEkle(int id,int adet)
+        {
+            string kulID = User.Identity.GetUserId(); //login olan kullanıcının idsini almak için
+            Sepet sepettekiurun=db.Sepet.FirstOrDefault(x=>x.UrunID==id && x.KullaniciID==kulID);
+            Urunler urun = db.Urunler.Find(id);
+            if (sepettekiurun==null)
+            {
+                Sepet yeniurun = new Sepet()
+                {
+                    KullaniciID = kulID,
+                    UrunID = id,
+                    Adet=adet,
+                    ToplamTutar=urun.UrunFiyati * adet
+                };
+                db.Sepet.Add(yeniurun);
+            }
+            else
+            {
+                sepettekiurun.Adet = sepettekiurun.Adet + adet;
+                sepettekiurun.ToplamTutar = sepettekiurun.Adet * urun.UrunFiyati;
+
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+           
+        }
+    }
+}
